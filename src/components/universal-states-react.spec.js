871e06f6567e023c1f38380fa4b9f204dee27594
intai <intai.hg@gmail.com>
2016-08-26 00:04:57 +1200

    Install enzyme

--- src/components/universal-states-react.spec.js
+++ src/components/universal-states-react.spec.js
@@ -1,20 +1,14 @@
-import chai from 'chai';
-import sinon from 'sinon';
-import React from 'react';
-import TestUtils from 'react-addons-test-utils';
-import { UniversalStates } from './universal-states-react';
-
-const render = () => {
-  let renderer = TestUtils.createRenderer();
-  renderer.render(<UniversalStates />);
-  return renderer.getRenderOutput();
-};
+import chai from 'chai'
+import sinon from 'sinon'
+import React from 'react'
+import { render, shallow } from 'enzyme'
+import { UniversalStates } from './universal-states-react'
 
 describe('UniversalStates Component', () => {
 
   it('should have id', () => {
-    let output = render();
-    chai.expect(output.props.id).to.equal('universal');
-  });
+    const wrapper = shallow(<UniversalStates />)
+    chai.expect(wrapper.props('id')).to.equal('universal')
+  })
 
-});
+})
