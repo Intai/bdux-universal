58860ce94af5023bbd4fe45320f00110382a7579
intai <intai.hg@gmail.com>
2016-08-28 00:24:52 +1200

    Add unit tests for common utilities

--- src/utils/common-util.js
+++ src/utils/common-util.js
@@ -1,23 +1,25 @@
-import R from 'ramda';
+import R from 'ramda'
 
-const PREFIX = 'IS';
+const PREFIX = 'IS'
+
+export const canUseDOM = () => (
+  typeof window !== 'undefined'
+    && window.document
+    && window.document.createElement
+)
 
 const mapToKeyValue = (obj, key) => {
-  obj[key] = PREFIX + '_' + key;
+  obj[key] = PREFIX + '_' + key
   return obj
-};
+}
 
 export default {
 
-  canUseDOM: R.once(() => (
-    typeof window !== 'undefined'
-      && window.document
-      && window.document.createElement
-  )),
+  canUseDOM: R.once(canUseDOM),
 
   // map an array of strings to
   // object keys and prefixed values.
-  createObjOfConsts: R.reduce(
-    mapToKeyValue, {}
+  createObjOfConsts: (values) => R.reduce(
+    mapToKeyValue, {}, values
   )
-};
+}
---
+++ src/utils/common-util.spec.js
@@ -0,0 +1,35 @@
+import chai from 'chai'
+import sinon from 'sinon'
+import Common, {
+  canUseDOM } from './common-util'
+
+describe('Common Utilities', () => {
+
+  it('should not be able to use dom when there is no window', () => {
+    global.window = undefined
+    chai.expect(canUseDOM()).to.not.be.ok
+  })
+
+  it('should not be able to use dom when there is no document', () => {
+    global.window = { document: undefined }
+    chai.expect(canUseDOM()).to.not.be.ok
+  })
+
+  it('should not be able to use dom when there is no function to create element', () => {
+    global.window = { document: { createElement: undefined }}
+    chai.expect(canUseDOM()).to.not.be.ok
+  })
+
+  it('should be able to use dom when there is function to create element', () => {
+    global.window = { document: { createElement: () => {} }}
+    chai.expect(canUseDOM()).to.be.ok
+  })
+
+  it('should generate an object of constants', () => {
+    const storeNames = Common.createObjOfConsts(['UNIVERSAL'])
+    chai.expect(storeNames).to.eql({
+      UNIVERSAL: 'IS_UNIVERSAL'
+    })
+  })
+
+})
